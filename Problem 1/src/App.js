534706474df8cc53155e8eb51e821af5963b7e6c
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-12-13 18:36:35 +0000

    Updated with relevant HTML from the note-taking app in week 1

--- Problem 1/src/App.js
+++ Problem 1/src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    	<div>
+        <div>
+		      <h3 class="heading_for_the_app">Note-Taking App</h3>
+	      </div><br></br>
+
+        <div class="AddNoteContainer">
+          <div class="AddNote">
+            <input type="text" id="title_note" name="title_note" placeholder="Enter the title"></input><br></br>
+
+            <textarea id="yournote" name="yournote" rows="4" cols="50"></textarea><br></br>
+			      <select name="colours" id="colours">
+				      <option value="red">Red</option>
+				      <option value="blue">Blue</option>
+				      <option value="green">Green</option>
+			      </select><br></br>
+			
+			      <button id="add_note_btn" onclick="button_action()">Add Note</button>
+
+          </div><br></br>
+        </div>
+
+
+        <div class="DisplayNoteContainer">
+		      <div id="DisplayNote">
+		      </div>
+	      </div>
+        
+      </div>
   );
 }
 
