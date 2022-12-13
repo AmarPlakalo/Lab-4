c05b77a1cd3feea0afef382444e429bd5803ff5b
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-12-13 18:54:58 +0000

    Added the javascript functions in the App.js
    These javascript functions perform adding, deleting and editing functionality.

--- Problem 1/src/App.js
+++ Problem 1/src/App.js
@@ -2,6 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function button_action()
+  {
+    let title_content = document.getElementById('title_note').value;
+    let note_content = document.getElementById('yournote').value;
+    let colour_content = document.getElementById('colours').value;
+    
+    const node = document.createElement("div");
+    
+    node.style.backgroundColor = colour_content;
+
+    var input = document.createElement("input");
+    input.type = "button";
+    input.className = "button";
+    input.value = "Edit Note";
+    
+    input.onclick = function () {
+      var title = window.prompt("Enter new title : ");
+        
+      var content = window.prompt("Enter new content : ");
+      
+      node.innerHTML = title + "<br><br><br>" + content;
+      node.appendChild(document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ));
+      node.appendChild(input);
+      node.appendChild(input_2);
+      
+    };
+
+    
+    var input_2 = document.createElement("input");
+    input_2.type = "button";
+    input_2.className = "button";
+    input_2.value = "Delete Note";
+    input_2.onclick = function () {
+      node.remove();
+    };
+    
+    node.innerHTML = title_content + "<br><br><br>" + note_content;
+    node.appendChild(document.createTextNode('\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ));
+    node.appendChild(input);
+    node.appendChild(input_2);
+
+    document.getElementById("DisplayNote").appendChild(node);
+    
+  }
+
+
   return (
     	<div>
         <div>
@@ -19,7 +66,7 @@ function App() {
 				      <option value="green">Green</option>
 			      </select><br></br>
 			
-			      <button id="add_note_btn" onclick="button_action()">Add Note</button>
+			      <button id="add_note_btn" onClick={button_action}>Add Note</button>
 
           </div><br></br>
         </div>
